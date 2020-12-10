536b6ddeb532ca4d918f1f29572ad1c63ff17891
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-12-10 13:12:47 -0600

    adding code to make images show up

--- crossingpaths/urls.py
+++ crossingpaths/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from rest_framework import routers
 from crossingpathsapi.views import register_user, login_user
@@ -34,4 +36,4 @@ urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
-]
+] + static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

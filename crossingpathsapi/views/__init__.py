3b6bfa77c85dfccfd319fe241ad15a8935dfb1e9
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-12-09 18:53:29 -0600

    fixed errors from copy pasta

--- crossingpathsapi/views/__init__.py
+++ crossingpathsapi/views/__init__.py
@@ -1,5 +1,6 @@
 from .auth import login_user, register_user
 from .category import Categories
 from .crossinguser import CrossingUsers
+from .currentuser import CurrentUser
 from .design import Designs
 from .friend import Follows
--- crossingpathsapi/views/currentuser.py
+++ crossingpathsapi/views/currentuser.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework import serializers, status
 from rest_framework.response import Response
-from crossingpathsapi.models import RareUser
+from crossingpathsapi.models import CrossingUser
 from crossingpathsapi.views.crossinguser import CrossingUserSerializer
 
 class CurrentUser(ViewSet):
@@ -15,7 +15,7 @@ class CurrentUser(ViewSet):
         """ handles GET currently logged in user """
 
         #the code in the parentheses is like a WHERE clause in SQL
-        user = RareUser.objects.get(user=request.auth.user)
+        user = CrossingUser.objects.get(user=request.auth.user)
 
         #imported the RareUserSerializer from rareuser.py to use in this module
         serializer = CrossingUserSerializer(user, many=False, context={'request': request})

5292aff53b9ee9ce3070ee1d8fbe1bf9b9bbf654
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-12-09 18:34:32 -0600

    added currentuser viewset

---
+++ crossingpathsapi/views/currentuser.py
@@ -0,0 +1,22 @@
+""" CurrentUser ViewSet Module"""
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from crossingpathsapi.models import RareUser
+from crossingpathsapi.views.crossinguser import CrossingUserSerializer
+
+class CurrentUser(ViewSet):
+    """RareUser Class"""
+
+    def list(self, request):
+        """ handles GET currently logged in user """
+
+        #the code in the parentheses is like a WHERE clause in SQL
+        user = RareUser.objects.get(user=request.auth.user)
+
+        #imported the RareUserSerializer from rareuser.py to use in this module
+        serializer = CrossingUserSerializer(user, many=False, context={'request': request})
+        return Response(serializer.data)
